9c43fbfbe940d6c1e7457fa5f8992cb5899887d3
krasnym <maciej.krasny@snpgroup.com>
2020-05-03 23:15:08 +0200

    Add navigation between views

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Products from "./views/Products.view";
+import Details from "./views/Details.view";
+import Cart from "./views/Cart.view";
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Switch>
+        <Route exact path={"/products"} component={Products} />
+        <Route exact path="/products/:id" component={Details} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
+    </Router>
   );
 }
 
